7cdba43adbd2edee78c0cec0d82a84b22fedd002
mkhodroo <info@behin.energy>
2024-09-07 12:59:54 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +21,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('build-app', function(){
+    Artisan::call('migrate');
+    return 'done';
 });
 
 require __DIR__.'/auth.php';
